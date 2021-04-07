a027ca4901f00e76a522b9b3d40dc85348c049b0
Cihan Sevim <earjsd@hotmail.com>
2021-04-07 13:13:42 +0300

    YGYK - 3. Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -23,7 +23,7 @@ namespace DegerVeReferansTipler
             //sayilar1 = sayilar2;
             //sayilar2[0] = 999;
 
-            //Console.WriteLine(sayilar1[0]);
+            //Console.WriteLine(sayilar1[0]); 
         }
     }
 }
