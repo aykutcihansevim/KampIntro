35ba00906a6f730fc2a8228739a3320a75981c3e
Cihan Sevim <earjsd@hotmail.com>
2021-04-08 12:12:34 +0300

    YGYK - 5. Ders Sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Business Classes - Work Rules
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if(musteri.)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //Interface birbirinin alternatifi olan fakat kod içerikleri farklı olan durumlar için kullanılır.
+    public interface IKrediManager
+    {
+        void Hesapla();
+
+        void BiseyYap();
+       
+    }
+}
