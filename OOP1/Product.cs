d2eb22759fb5cd0980bd63b9a4a6e22c3762d92d
Cihan Sevim <earjsd@hotmail.com>
2021-04-08 10:28:36 +0300

    YGYK - 4. Ders Sonu

--- OOP1/Product.cs
+++ OOP1/Product.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace OOP1
 {
     public class Product
-    {
+    { 
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
